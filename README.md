5e29e0417a6e3ce8c9a3446041778f97e59a7cb9
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-10-12 23:31:08 +0200

    Initial commit - setup app barebones

---
+++ README.md
@@ -0,0 +1,15 @@
+### BMI Calculator App
+
+
+#### Description
+
+BMI calculator app created in Flutter.
+
+#### TODO's
+
+- ... we will see. :) 
+
+
+#### App preview
+
+TODO
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('App body'),
+      ),
+    );
+  }
+}
