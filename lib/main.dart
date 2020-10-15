1160fd296fa542679af5215749764da7db8b13d9
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-10-15 22:43:52 +0200

    Change primary colors of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calc/pages/home.dart';
+import 'consts.dart';
 
 void main() {
   runApp(App());
@@ -9,7 +10,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: scaffoldBackgroudColor,
+        appBarTheme: AppBarTheme(color: appBarColor),
+      ),
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
